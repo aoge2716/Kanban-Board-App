396a8f9916cd1569b4333d2a7d1b1e2c6f6fc0f1
aoge2716 <aoge2716@gmail.com>
2025-02-04 16:37:22 +0100

    display information of json in home.jsx

--- kanBan/src/component/Home.jsx
+++ kanBan/src/component/Home.jsx
@@ -1,14 +1,24 @@
 import Sidebar from "./SideBar.jsx"
 import Navbar from "./Navbar.jsx"
 import Footer from "./Footer.jsx"
-
+import tasksArr from "../assets/json/ItemsList.json"
 export default function Home(){
     return(
         <div id="home">
+            
         
         <Sidebar/>
         <Navbar/>
         <h1>HOME PAGE</h1>
+
+        <ul>
+            {tasksArr.map(element => {
+                return(
+                <li>{element.title}:{element.status}</li>
+            )
+            })}
+        </ul>
+
         <Footer/>
         </div>
     )
