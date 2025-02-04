55d216d93c9c8cf5f824a49223d49c3e80b24c93
Antoine Quint <antoine.quint4@gmail.com>
2025-02-04 16:11:58 +0100

    Added the link to repo in footer and the name and logo to navbar

--- kanBan/src/component/Footer.jsx
+++ kanBan/src/component/Footer.jsx
@@ -1,3 +1,3 @@
 export default function Footer(){
-    
+    return <a href="https://github.com/aoge2716/Kanban-Board-App">Our repository</a>
 }
--- kanBan/src/component/Navbar.jsx
+++ kanBan/src/component/Navbar.jsx
@@ -1,3 +1,13 @@
-export default function Navbar(){
-    
-}
+export default function Navbar() {
+    return (
+      <div className="topnav" style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+      }}>
+        <h1>Kanbanana</h1>
+        <img src="./src/assets/p6.jpg" alt="A banana" style={{ width: '4vw' }}/>
+      </div>
+    );
+  }
