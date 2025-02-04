a23cfc82cab770e23780505ba3ddd1fb2ff945c6
Antoine Quint <antoine.quint4@gmail.com>
2025-02-04 17:20:34 +0100

    Added some styling and relative positions

--- kanBan/src/component/Navbar.jsx
+++ kanBan/src/component/Navbar.jsx
@@ -1,14 +1,9 @@
 export default function Navbar() {
     return (
     
-      <div className="topnav" style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-      }}>
+      <div id="topnav">
         <h1>Kanbanana</h1>
-        <img src="./src/assets/p6.jpg" alt="A banana" style={{ width: '4vw' }}/>
+        <img src="./src/assets/p6.jpg" alt="A banana"/>
       </div>
     );
   }
