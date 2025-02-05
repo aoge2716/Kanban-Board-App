202130beb2cad7cc0c3d69e7f39113f51c1e389b
aoge2716 <aoge2716@gmail.com>
2025-02-05 17:01:02 +0100

    implemented and improved the List Component

--- kanBan/src/component/List.jsx
+++ kanBan/src/component/List.jsx
@@ -14,29 +14,55 @@ export default function List(props){
     }
     
 
-
-
-
     return (
-        <ul>
+        <div className="tasks">
             {tasksToDisplay.map(element => {
-                
                 return(
-                    <div className="tasks" key={element.id}>
-                        <li>title:{element.title}</li>
+                    <div className="task" key={element.id}>
+                        <ul>
+                        <ListItem title={element.title} description={element.description} 
+                        assignee={element.assignee} priority={element.priority}
+                        createdDate={element.createdDate} status={element.status}/>
+                        {/* <li>title:{element.title}</li>
                         <li>description: {element.description}</li>
                         <li>assignee: {element.assignee}</li>
                         <li>priority: {element.priority}</li>
                         <li>created date: {element.createdDate}</li>
-                        <li>status:{element.status}
-                            <img src={`${element.status==="Done"
+                        <li>status:{element.status}</li> */}
+                        <img src={`${element.status==="Done"
                             ? "./src/assets/image/status/heavy_check_mark.png"
                             : "./src/assets/image/status/x.png"}`} alt="" />
-                        </li>
                         <button onClick={()=>clicked(element.id)}>delete</button>
+                        </ul>
                     </div>
             )
             })}
-        </ul>
+        </div>
+        
+            
     )
+
+
+    // return (
+    //     <ul>
+    //         {tasksToDisplay.map(element => {
+                
+    //             return(
+    //                 <div className="tasks" key={element.id}>
+    //                     <li>title:{element.title}</li>
+    //                     <li>description: {element.description}</li>
+    //                     <li>assignee: {element.assignee}</li>
+    //                     <li>priority: {element.priority}</li>
+    //                     <li>created date: {element.createdDate}</li>
+    //                     <li>status:{element.status}
+    //                         <img src={`${element.status==="Done"
+    //                         ? "./src/assets/image/status/heavy_check_mark.png"
+    //                         : "./src/assets/image/status/x.png"}`} alt="" />
+    //                     </li>
+    //                     <button onClick={()=>clicked(element.id)}>delete</button>
+    //                 </div>
+    //         )
+    //         })}
+    //     </ul>
+    // )
 }
--- kanBan/src/component/ListItem.jsx
+++ kanBan/src/component/ListItem.jsx
@@ -5,19 +5,14 @@ export default function ListItem(props){
  
     return (
 
-        <div className="tasks" key={props.id}>
+        <div  key={props.id}>
             <ul>
                 <li>title:{props.title}</li>
                 <li>description: {props.description}</li>
                 <li>assignee: {props.assignee}</li>
                 <li>priority: {props.priority}</li>
                 <li>created date: {props.createdDate}</li>
-                <li>status:{props.status}
-                    <img src={`${props.status==="Done"
-                    ? "./src/assets/image/status/heavy_check_mark.png"
-                    : "./src/assets/image/status/x.png"}`} alt="" />
-                </li>
-                <button onClick={()=>clicked(props.id)}>delete</button>
+                <li>status:{props.status}</li>
             </ul>
         </div>
             
