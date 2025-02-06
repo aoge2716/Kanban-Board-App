42675835ac75c189b7639907282fffc63f13d122
Antoine Quint <antoine.quint4@gmail.com>
2025-02-06 16:54:33 +0100

    Added AboutPage and it's route to sidebar

---
+++ kanBan/src/component/AboutPage.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Link, NavLink } from "react-router-dom";import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Our Project</h1>
+      <p>We are very passionate about doing a kanban thing</p>
+      <p>Very very passionate</p>
+    </div>
+  );
+}
