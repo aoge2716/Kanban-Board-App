e08f14046f146574dc20f65243779198f33f1dfa
aoge2716 <aoge2716@gmail.com>
2025-02-12 17:58:17 +0100

    img path fix

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 export default function Footer(){
     return (
         <div id="footer">
-            <img src="../../public/image/logo/git_logo.png" alt="githublogo" />
+            <img src="/image/logo/git_logo.png" alt="githublogo" />
             <a href="https://github.com/aoge2716/Kanban-Board-App">Our repository</a>
         </div>
     )
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom";
 import "./Navbar.css"
 
+
 export default function Navbar(props) {
     return (
     
